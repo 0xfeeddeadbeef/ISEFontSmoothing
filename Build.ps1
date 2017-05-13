32c7f4d4b548b3cd3a12b9eb78dd30357683bde7
George Chakhidze <0xfeeddeadbeef@gmail.com>
2017-05-13 18:41:18 +0400

    Add simple build script

---
+++ Build.ps1
@@ -0,0 +1,20 @@
+﻿param (
+    [string] $OutDir = '.\bld',
+    [switch] $Publish,
+    [string] $ApiKey
+)
+
+if (-not (Test-Path $OutDir))
+{
+    New-Item -ItemType Directory -Path $OutDir -Verbose | Out-Null
+}
+
+Copy-Item -Path @('.\*.psd1','.\*.psm1') -Destination $OutDir -Force -Verbose
+
+if ($Publish)
+{
+    Import-Module PowerShellGet
+
+    Publish-Module -Name (Resolve-Path $OutDir) -NuGetApiKey $ApiKey -Verbose
+}
+
